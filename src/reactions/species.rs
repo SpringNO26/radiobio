b69e25b4edd7e599c0b007a5ee26701ec77f5bd7
Spring <SpringJet@protonmail.me>
2023-02-17 23:36:49 +0100

    Impl operation: Species . f64

--- src/reactions/species.rs
+++ src/reactions/species.rs
@@ -42,16 +42,20 @@ impl<'a, 'b> Mul<&'b Species> for &'a Species {
     }
 }
 
+impl Mul<f64> for &Species {
+    type Output = f64;
+    fn mul(self, other:f64) -> f64 {
+        self.cc * other
+    }
+}
+
 impl<'a, 'b> Sub<&'b Species> for &'a Species {
     type Output = f64;
-    fn sub(self, other:&'b Species) -> f64 {
-        let res = self.cc - other.cc;
-        // Calculated cc cannot be negative.
-        if res <= 0.0 {
+    fn sub(self, rhs:&'b Species) -> f64 {
+        if self.cc <= rhs.cc {
             return 0 as f64;
         } else {
-            return res;
+            return self.cc - rhs.cc;
         }
-
     }
 }
